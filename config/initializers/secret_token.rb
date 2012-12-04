6d40ac40149438053b82e23dec49ce607e3e4b2c
Vishnu R Menon <vishthemenon@gmail.com>
2012-12-04 15:49:47 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leposse::Application.config.secret_token = '3640fb8831ff8bb5054476a55c8f0d7471ac114bbb158e1727732ca7866b61c46a6cfd6eeff8d6f92f4a6b9d45cb493f5724654df8370040e0a3b7215e3f9fa2'
