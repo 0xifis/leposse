5d761424c9701f795ed3fc1a08f7c73054d7bd8d
Jethro Kuan <jethrokuan95@gmail.com>
2012-12-07 04:16:41 +0800

    prettified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Leposse::Application.routes.draw do
-
-  
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   resources :venues
-
-
   resources :games
 
-
-  get "pages/home"
-
-  get "pages/about"
+  match "/about" => "pages#about", as: :about
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
 
   root to: "pages#home"
 
